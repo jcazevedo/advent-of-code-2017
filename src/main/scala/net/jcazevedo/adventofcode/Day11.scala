9c3ae4a40e7332171decf3f402a0b4026a834179
Joao Azevedo <joao.c.azevedo@gmail.com>
2017-12-30 20:21:40 +0000

    Add 'Day11' solution

---
+++ src/main/scala/net/jcazevedo/adventofcode/Day11.scala
@@ -0,0 +1,30 @@
+package net.jcazevedo.adventofcode
+
+object Day11 extends App with AdventOfCode {
+  case class Cube(x: Int, y: Int, z: Int)
+
+  def move(cube: Cube, dir: String): Cube =
+    dir match {
+      case "n" => cube.copy(y = cube.y + 1, z = cube.z - 1)
+      case "ne" => cube.copy(x = cube.x + 1, z = cube.z - 1)
+      case "se" => cube.copy(x = cube.x + 1, y = cube.y - 1)
+      case "s" => cube.copy(y = cube.y - 1, z = cube.z + 1)
+      case "sw" => cube.copy(x = cube.x - 1, z = cube.z + 1)
+      case "nw" => cube.copy(x = cube.x - 1, y = cube.y + 1)
+    }
+
+  def dist(start: Cube, end: Cube): Int =
+    (math.abs(start.x - end.x) + math.abs(start.y - end.y) + math.abs(start.z - end.z)) / 2
+
+  val line = loadFile(11)
+  val directions = line.head.split(",")
+  val start = Cube(0, 0, 0)
+
+  val (end, maxDist) = directions.foldLeft((start, 0)) {
+    case ((currP, currDist), coord) =>
+      val next = move(currP, coord)
+      (next, math.max(currDist, dist(next, start)))
+  }
+  println(dist(end, start))
+  println(maxDist)
+}
