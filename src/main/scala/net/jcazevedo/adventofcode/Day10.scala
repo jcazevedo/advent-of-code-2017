a75781f550c07a56391e184b014396b263d12deb
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-12-01 11:01:06 +0000

    Make day 10 use the DailyChallenge trait

--- src/main/scala/net/jcazevedo/adventofcode/Day10.scala
+++ src/main/scala/net/jcazevedo/adventofcode/Day10.scala
@@ -4,26 +4,33 @@ class Day10 extends DailyChallenge[Int, String] {
   def run(filename: String): (Int, String) = {
     val line = io.Source.fromFile(filename).getLines.toList
 
+    def reverse(ns: IndexedSeq[Int], i: Int, j: Int): IndexedSeq[Int] =
+      if (i > j)
+        ns
+      else {
+        val ni = i % ns.length
+        val nj = j % ns.length
+        reverse(ns.updated(nj, ns(ni)).updated(ni, ns(nj)), i + 1, j - 1)
+      }
+
+    def go(ns: IndexedSeq[Int], ls: Seq[Int], position: Int = 0, skipSize: Int = 0): (IndexedSeq[Int], Int, Int) =
+      if (ls.isEmpty)
+        (ns, position, skipSize)
+      else
+        go(reverse(ns, position, position + ls.head - 1), ls.tail, position + ls.head + skipSize, skipSize + 1)
+
+    def xorAll(l: Seq[Int]): Int =
+      if (l.size == 1) l.head
+      else l.head ^ xorAll(l.tail)
+
+    def firstTwo(input: String): Int = {
+      val numbers = (0 to 255)
+      val lengths = input.split(",").map(_.toInt)
+      val res = go(numbers, lengths)._1
+      res(0) * res(1)
+    }
+
     def knotHash(input: String): String = {
-      def reverse(ns: IndexedSeq[Int], i: Int, j: Int): IndexedSeq[Int] =
-        if (i > j)
-          ns
-        else {
-          val ni = i % ns.length
-          val nj = j % ns.length
-          reverse(ns.updated(nj, ns(ni)).updated(ni, ns(nj)), i + 1, j - 1)
-        }
-
-      def go(ns: IndexedSeq[Int], ls: Seq[Int], position: Int = 0, skipSize: Int = 0): (IndexedSeq[Int], Int, Int) =
-        if (ls.isEmpty)
-          (ns, position, skipSize)
-        else
-          go(reverse(ns, position, position + ls.head - 1), ls.tail, position + ls.head + skipSize, skipSize + 1)
-
-      def xorAll(l: Seq[Int]): Int =
-        if (l.size == 1) l.head
-        else l.head ^ xorAll(l.tail)
-
       val numbers = (0 to 255)
       val lengths = input.map(_.toInt) ++ Seq(17, 31, 73, 47, 23)
 
@@ -36,6 +43,6 @@ class Day10 extends DailyChallenge[Int, String] {
       sparseHash.sliding(16, 16).map(xorAll).map(_.toHexString).mkString
     }
 
-    (-1, knotHash(line.head))
+    (firstTwo(line.head), knotHash(line.head))
   }
 }
