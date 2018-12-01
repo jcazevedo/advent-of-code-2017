0b2c9b6dfc3aa473e69f2b411bd0f9ca45eb8560
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-12-01 11:03:45 +0000

    Make day 11 use the DailyChallenge trait

--- src/main/scala/net/jcazevedo/adventofcode/Day11.scala
+++ src/main/scala/net/jcazevedo/adventofcode/Day11.scala
@@ -1,30 +1,31 @@
 package net.jcazevedo.adventofcode
 
-object Day11 extends App with AdventOfCode {
-  case class Cube(x: Int, y: Int, z: Int)
+class Day11 extends DailyChallenge[Int, Int] {
+  def run(filename: String): (Int, Int) = {
+    case class Cube(x: Int, y: Int, z: Int)
 
-  def move(cube: Cube, dir: String): Cube =
-    dir match {
-      case "n" => cube.copy(y = cube.y + 1, z = cube.z - 1)
-      case "ne" => cube.copy(x = cube.x + 1, z = cube.z - 1)
-      case "se" => cube.copy(x = cube.x + 1, y = cube.y - 1)
-      case "s" => cube.copy(y = cube.y - 1, z = cube.z + 1)
-      case "sw" => cube.copy(x = cube.x - 1, z = cube.z + 1)
-      case "nw" => cube.copy(x = cube.x - 1, y = cube.y + 1)
+    def move(cube: Cube, dir: String): Cube =
+      dir match {
+        case "n" => cube.copy(y = cube.y + 1, z = cube.z - 1)
+        case "ne" => cube.copy(x = cube.x + 1, z = cube.z - 1)
+        case "se" => cube.copy(x = cube.x + 1, y = cube.y - 1)
+        case "s" => cube.copy(y = cube.y - 1, z = cube.z + 1)
+        case "sw" => cube.copy(x = cube.x - 1, z = cube.z + 1)
+        case "nw" => cube.copy(x = cube.x - 1, y = cube.y + 1)
+      }
+
+    def dist(start: Cube, end: Cube): Int =
+      (math.abs(start.x - end.x) + math.abs(start.y - end.y) + math.abs(start.z - end.z)) / 2
+
+    val lines = io.Source.fromFile(filename).getLines().toList
+    val directions = lines.head.split(",")
+    val start = Cube(0, 0, 0)
+
+    val (end, maxDist) = directions.foldLeft((start, 0)) {
+      case ((currP, currDist), coord) =>
+        val next = move(currP, coord)
+        (next, math.max(currDist, dist(next, start)))
     }
-
-  def dist(start: Cube, end: Cube): Int =
-    (math.abs(start.x - end.x) + math.abs(start.y - end.y) + math.abs(start.z - end.z)) / 2
-
-  val line = loadFile(11)
-  val directions = line.head.split(",")
-  val start = Cube(0, 0, 0)
-
-  val (end, maxDist) = directions.foldLeft((start, 0)) {
-    case ((currP, currDist), coord) =>
-      val next = move(currP, coord)
-      (next, math.max(currDist, dist(next, start)))
+    (dist(end, start), maxDist)
   }
-  println(dist(end, start))
-  println(maxDist)
 }
