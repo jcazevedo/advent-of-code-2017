b3e2781d97c2779cde88cb8d0c077e0fe3bb589c
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-12-01 11:07:53 +0000

    Make day 13 use the DailyChallenge trait

--- src/main/scala/net/jcazevedo/adventofcode/Day13.scala
+++ src/main/scala/net/jcazevedo/adventofcode/Day13.scala
@@ -1,47 +1,52 @@
 package net.jcazevedo.adventofcode
 
-object Day13 extends App with AdventOfCode {
-  case class Scanner(range: Int, position: Int, direction: Int = -1) {
-    def move: Scanner = {
-      val nextDir = if (position == (range - 1) || position == 0) -direction else direction
-      this.copy(position = position + nextDir, direction = nextDir)
+class Day13 extends DailyChallenge[Int, Int] {
+  def run(filename: String): (Int, Int) = {
+    case class Scanner(range: Int, position: Int, direction: Int = -1) {
+      def move: Scanner = {
+        val nextDir = if (position == (range - 1) || position == 0) -direction else direction
+        this.copy(position = position + nextDir, direction = nextDir)
+      }
     }
-  }
 
-  val scanners: Map[Int, Scanner] = loadFile(13).map { l =>
-    val Array(depth, range) = l.split(":")
-    depth.trim.toInt -> Scanner(range.trim.toInt, 0)
-  }.toMap
+    val lines = io.Source.fromFile(filename).getLines().toList
 
-  def moveAll(scanners: Map[Int, Scanner]): Map[Int, Scanner] =
-    scanners.map { case (k, v) => k -> v.move }
+    val scanners: Map[Int, Scanner] = lines.map { l =>
+      val Array(depth, range) = l.split(":")
+      depth.trim.toInt -> Scanner(range.trim.toInt, 0)
+    }.toMap
 
-  val maxDepth = scanners.keySet.max
-  def severity(from: Int, to: Int, scanners: Map[Int, Scanner]): Int =
-    if (from > to)
-      0
-    else
-      scanners.get(from) match {
-        case None => severity(from + 1, to, moveAll(scanners))
-        case Some(scanner) =>
-          val currSeverity = if (scanner.position == 0) from * scanner.range else 0
-          currSeverity + severity(from + 1, to, moveAll(scanners))
+    def moveAll(scanners: Map[Int, Scanner]): Map[Int, Scanner] =
+      scanners.map { case (k, v) => k -> v.move }
+
+    val maxDepth = scanners.keySet.max
+
+    def severity(from: Int, to: Int, scanners: Map[Int, Scanner]): Int =
+      if (from > to)
+        0
+      else
+        scanners.get(from) match {
+          case None => severity(from + 1, to, moveAll(scanners))
+          case Some(scanner) =>
+            val currSeverity = if (scanner.position == 0) from * scanner.range else 0
+            currSeverity + severity(from + 1, to, moveAll(scanners))
+        }
+
+    def gotCaught(from: Int, to: Int, scanners: Map[Int, Scanner]): Boolean =
+      if (from > to)
+        false
+      else scanners.get(from) match {
+        case Some(scanner) if scanner.position == 0 => true
+        case _ => gotCaught(from + 1, to, moveAll(scanners))
       }
 
-  def gotCaught(from: Int, to: Int, scanners: Map[Int, Scanner]): Boolean =
-    if (from > to)
-      false
-    else scanners.get(from) match {
-      case Some(scanner) if scanner.position == 0 => true
-      case _ => gotCaught(from + 1, to, moveAll(scanners))
+    def it = Iterator.iterate((0, scanners)) { case (n, scanners) => (n + 1, moveAll(scanners)) }
+
+    def sols = it.map {
+      case (n, initialScanners) =>
+        n -> gotCaught(0, maxDepth, initialScanners)
     }
 
-  def it = Iterator.iterate((0, scanners)) { case (n, scanners) => (n + 1, moveAll(scanners)) }
-  def sols = it.map {
-    case (n, initialScanners) =>
-      n -> gotCaught(0, maxDepth, initialScanners)
+    (severity(0, maxDepth, scanners), sols.dropWhile(_._2).next._1)
   }
-
-  println(severity(0, maxDepth, scanners))
-  println(sols.dropWhile(_._2).next._1)
 }
