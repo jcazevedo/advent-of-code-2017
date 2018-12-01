f556a3dbee9f6ae142dc36a00102dfc9e23815a8
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-12-01 11:05:17 +0000

    Make day 12 use the DailyChallenge trait

--- src/main/scala/net/jcazevedo/adventofcode/Day12.scala
+++ src/main/scala/net/jcazevedo/adventofcode/Day12.scala
@@ -1,32 +1,35 @@
 package net.jcazevedo.adventofcode
 
-object Day12 extends App with AdventOfCode {
-  val neighbors: Map[Int, List[Int]] = loadFile(12).map { l =>
-    val Array(from, to) = l.split("<->")
-    from.trim.toInt -> to.split(",").map(_.trim.toInt).toList
-  }.toMap
+class Day12 extends DailyChallenge[Int, Int] {
+  def run(filename: String): (Int, Int) = {
+    val lines = io.Source.fromFile(filename).getLines().toList
 
-  def groupOf(curr: Int, neighbors: Map[Int, List[Int]], visited: Set[Int] = Set()): Set[Int] =
-    if (visited.contains(curr))
-      visited
-    else {
-      val nVisited = visited + curr
-      neighbors(curr).foldLeft(nVisited) {
-        case (currVisited, neigh) =>
-          currVisited ++ groupOf(neigh, neighbors, currVisited)
+    val neighbors: Map[Int, List[Int]] = lines.map { l =>
+      val Array(from, to) = l.split("<->")
+      from.trim.toInt -> to.split(",").map(_.trim.toInt).toList
+    }.toMap
+
+    def groupOf(curr: Int, neighbors: Map[Int, List[Int]], visited: Set[Int] = Set()): Set[Int] =
+      if (visited.contains(curr))
+        visited
+      else {
+        val nVisited = visited + curr
+        neighbors(curr).foldLeft(nVisited) {
+          case (currVisited, neigh) =>
+            currVisited ++ groupOf(neigh, neighbors, currVisited)
+        }
       }
+
+    val (_, nGroups) = neighbors.foldLeft((Set.empty[Int], 0)) {
+      case ((visited, nGroups), (node, _)) =>
+        if (!visited.contains(node)) {
+          val nextVisited = visited ++ groupOf(node, neighbors)
+          (nextVisited, nGroups + 1)
+        } else {
+          (visited, nGroups)
+        }
     }
 
-  val (_, nGroups) = neighbors.foldLeft((Set.empty[Int], 0)) {
-    case ((visited, nGroups), (node, _)) =>
-      if (!visited.contains(node)) {
-        val nextVisited = visited ++ groupOf(node, neighbors)
-        (nextVisited, nGroups + 1)
-      } else {
-        (visited, nGroups)
-      }
+    (groupOf(0, neighbors).size, nGroups)
   }
-
-  println(groupOf(0, neighbors).size)
-  println(nGroups)
 }
